9ffbdc586698ac9269668c6143eee582a063886f
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-09-24 12:28:13 +0600

    Implemented printWord function on NumWordPrinter class

--- oopLab_3/src/NumWordPrinter.java
+++ oopLab_3/src/NumWordPrinter.java
@@ -1,3 +1,10 @@
 public class NumWordPrinter {
-    
+    public String printWord(int n){
+        if(n%2==0 && n%11==0 && n%13==0) return "EvenPrimeLucky";
+        if(n%2==0 && n%11==0) return "EvenPrimeEleven";
+        if(n%2==0) return "Even";
+        if(n%11==0) return "PrimeEleven";
+        if(n%13==0) return "LuckyThirteen";
+        return "OddOneOut";
+    }
 }

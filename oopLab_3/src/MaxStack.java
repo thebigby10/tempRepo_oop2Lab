f035e8a1b32347d99f9188f05f3bacbe25e4c435
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2024-09-24 12:48:26 +0600

    Implemented MaxStack.java

--- oopLab_3/src/MaxStack.java
+++ oopLab_3/src/MaxStack.java
@@ -1,3 +1,24 @@
+import java.util.Stack;
+
 public class MaxStack {
+    Stack<Integer >stack = new Stack<Integer >();
+    Stack<Integer> max_elements = new Stack<Integer>();
+    // int max_element = -1;
+    public void push(int n){
+        stack.push(n);
+        if(max_elements.peek()<=n) max_elements.push(n);
+    }
+    public int pop(){
+        if(!stack.isEmpty()){
+            if(stack.peek()==max_elements.peek()){
+                max_elements.pop();
+                return max_elements.pop();
+            }
+        }
+        return max_elements.pop();
+    }
+    public int max(){
+        return max_elements.peek();
+    }
     
 }
